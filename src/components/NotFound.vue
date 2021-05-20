b484210f9e6cf83a5516e1499d49ef71ca2a51a0
marcos-moreno <45862618+marcos-moreno@users.noreply.github.com>
2021-05-19 20:12:29 -0500

    Tender image

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -4,7 +4,7 @@
     <v-container class="mx-auto text-center" style="min-height:706px;">
       <br><br><br>
       <!-- width="280" -->
-      <img  src="https://www.karmaestudio.com/wp-content/uploads/2015/01/Mantenimiento-paginas-web.png">
+      <img width="80%" src="https://www.karmaestudio.com/wp-content/uploads/2015/01/Mantenimiento-paginas-web.png">
       <p style="font-size: 2em;color :#909090">  
         <strong>La página que buscas aún no esta disponible.</strong> 
       </p>
