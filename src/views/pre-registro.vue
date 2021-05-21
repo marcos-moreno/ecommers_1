73ccb4f5bb07dd4bae47d49f28673b6132ec32d5
marcos-moreno <marcos.moreno.gm@gmail.com>
2021-05-21 17:32:56 -0500

    BD real

--- src/views/pre-registro.vue
+++ src/views/pre-registro.vue
@@ -783,7 +783,7 @@ export default {
         acuse(){  
            var doc = new jsPDF('p', 'pt',[700, 380]);
             var img = new Image()
-            img.src = '/refividrioR.png';   
+            img.src = '/refivid.png';   
             doc.addImage(img,'png', 95, 10, 0, 0);   
             doc.setTextColor(0, 106, 164 );
             doc.setFontSize(10);
