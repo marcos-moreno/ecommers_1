1267378e745e0368630004dec20a10a535511209
marcos-moreno <marcos.moreno.gm@gmail.com>
2021-05-21 17:36:31 -0500

    BD real

--- src/components/admin/Pre-registros.vue
+++ src/components/admin/Pre-registros.vue
@@ -416,6 +416,7 @@ export default {
         },
         refresh(){
             this.getCollections();
+            this.filtrarRegistros();
         },
         async crud(item){
             this.msgError = "";
