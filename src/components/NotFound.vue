3d6c34755248e06b3956f4154b83d35826a908e0
marcos-moreno <45862618+marcos-moreno@users.noreply.github.com>
2021-06-29 10:06:42 -0500

    Update NotFound.vue

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -4,7 +4,7 @@
     <v-container class="mx-auto text-center" style="min-height:706px;">
       <br><br><br>
       <!-- width="280" -->
-      <img width="80%" src="http://refividrio.com.mx/ecommerce/mantenimiento.png">
+      <img width="80%" src="http://refividrio.com.mx/ecommerce/mantenimiento12.png">
       
       <p style="font-size: 2em;color :#909090">  
         <strong>La página que buscas aún no esta disponible.</strong> 
