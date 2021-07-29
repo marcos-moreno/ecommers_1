6505f7de98ddfb187a4135210e5b00269c4a4f32
marcos-moreno <marcos.moreno.gm@gmail.com>
2021-07-29 13:05:33 -0500

    add component Capacitacionsensaodor

--- src/components/Capacitaciones.vue
+++ src/components/Capacitaciones.vue
@@ -3,7 +3,7 @@
     <app-menu/> 
     <v-container class="mx-auto text-center" style="min-height:706px;">
       <br><br><br> 
-      <p style="font-size: 1em;color :#909090">  
+      <p style="font-size: 1.3em;color :#909090">  
        <strong>CAPACITACIÓN AROMAS DE ANDALUCÍA.</strong> 
       </p>
       <iframe
@@ -11,6 +11,17 @@
         src="https://refividrio.com.mx/EcommerceDistribuidor/Videos/CAPACITACIÓNAROMASDEANDALUCIA.mp4" 
         frameborder="0" allowfullscreen="allowfullscreen">
       </iframe>
+
+      <br><br><br> 
+      <p style="font-size: 1.3em;color :#909090">  
+       <strong>CAPACITACIÓN SENSAODOR.</strong> 
+      </p>
+      <iframe
+      width="560" height="349"
+        src="https://refividrio.com.mx/EcommerceDistribuidor/Videos/CAPACITACIONSENSAODOR.mp4" 
+        frameborder="0" allowfullscreen="allowfullscreen">
+      </iframe>
+      <br><br><br> 
     </v-container>
   </div>
 </template> 
